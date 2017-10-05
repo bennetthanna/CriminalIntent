4d9669083a606d016761815966ba6b34be7e93ef
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-04 18:32:12 -0700

    Chapter 7, page 144 : Created CrimeFragment class and implemented onCreate and onCreateView

---
+++ app/src/main/java/com/hannabennett/criminalintent/CrimeFragment.java
@@ -0,0 +1,27 @@
+package com.hannabennett.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HannaBennett on 10/4/17.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+}

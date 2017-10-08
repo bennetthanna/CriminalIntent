575e42454c1d7111821f2e55398e02790c2cc7b1
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-08 11:40:51 -0700

    Chapter 8, page 175 : Set up adapter to connect to RecyclerView

--- app/src/main/java/com/hannabennett/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/hannabennett/criminalintent/CrimeListFragment.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +25,17 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
+
         return view;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder {

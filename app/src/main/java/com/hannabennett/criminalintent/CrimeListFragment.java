f4ea1acdbd10078c760ccc5da4386c1f9a6c272f
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-13 16:26:26 -0700

    Formatted crime_date to display weekday, month, day, and year instead of timestamp

--- app/src/main/java/com/hannabennett/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/hannabennett/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,7 +61,7 @@ public class CrimeListFragment extends Fragment {
         public void bind(Crime crime) {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
-            mDateTextView.setText(mCrime.getDate().toString());
+            mDateTextView.setText(DateFormat.format("EEEE, MMMM dd, yyyy", mCrime.getDate()).toString());
             mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 

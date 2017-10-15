69066e870b0fae0d42ce42de7cea1345e3006812
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-15 11:17:27 -0700

    Chapter 10, page 209 : CrimeFragment's view displays the fetched crime data

--- app/src/main/java/com/hannabennett/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/hannabennett/criminalintent/CrimeFragment.java
@@ -38,6 +38,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -60,6 +61,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {

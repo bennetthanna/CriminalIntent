53532851a21a28f8c64fdd5a3971d9416e46c927
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-17 10:33:20 -0700

    Chapter 11, page 218 : Created CrimePagerActivity and set up a ViewPager view

---
+++ app/src/main/java/com/hannabennett/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,17 @@
+package com.hannabennett.criminalintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HannaBennett on 10/17/17.
+ */
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}

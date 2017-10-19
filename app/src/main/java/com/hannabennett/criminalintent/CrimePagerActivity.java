a6b28880b1b32b34f073741326a02b6fdbb75e90
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-18 22:42:25 -0700

    Chapter 11, page 220 : Set up pager adapter

--- app/src/main/java/com/hannabennett/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/hannabennett/criminalintent/CrimePagerActivity.java
@@ -1,7 +1,13 @@
 package com.hannabennett.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 /**
  * Created by HannaBennett on 10/17/17.
@@ -9,9 +15,30 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CrimePagerActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }

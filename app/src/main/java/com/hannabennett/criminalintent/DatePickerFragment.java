1c46ee07e747d2974547ec16ee846b98073cb838
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-19 10:22:44 -0700

    Chapter 12, page 230 : Created a Dialog fragment

---
+++ app/src/main/java/com/hannabennett/criminalintent/DatePickerFragment.java
@@ -0,0 +1,20 @@
+package com.hannabennett.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by HannaBennett on 10/19/17.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}

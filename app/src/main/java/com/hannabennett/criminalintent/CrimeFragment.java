c3b3d3ce5a88029682c552db0e225e649c0abd0a
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-20 15:21:00 -0700

    Chapter 12, page 238 : Set CrimeFragment as the target fragment of DatePickerFragment

--- app/src/main/java/com/hannabennett/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/hannabennett/criminalintent/CrimeFragment.java
@@ -26,6 +26,8 @@ public class CrimeFragment extends Fragment {
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
 
+    private static final int REQUEST_DATE = 0;
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -77,6 +79,7 @@ public class CrimeFragment extends Fragment {
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
                 DatePickerFragment dialog = new DatePickerFragment().newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(manager, DIALOG_DATE);
             }
         });

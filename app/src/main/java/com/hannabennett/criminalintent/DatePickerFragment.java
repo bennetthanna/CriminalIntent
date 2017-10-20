2153b25600a7391bd7c516d68f9b77744a348200
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-20 15:15:40 -0700

    Chapter 12, page 237 : Extract date using integers from Calendar object to initialize DatePicker

--- app/src/main/java/com/hannabennett/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/hannabennett/criminalintent/DatePickerFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
+
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -31,7 +33,19 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year, month, day, null);
+
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle(R.string.date_picker_title)

6e2ea78a7b92166e34f91427b3046ae25d94cabc
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-24 15:21:37 -0700

    Chapter 14, page 273 : Create crime table

--- app/src/main/java/com/hannabennett/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/hannabennett/criminalintent/database/CrimeBaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.hannabennett.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 /**
  * Created by HannaBennett on 10/24/17.
  */
@@ -18,6 +20,12 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + CrimeTable.NAME + "(" +
+        " _id integer primary key autoincrement, " +
+        CrimeTable.Cols.UUID + ", " +
+        CrimeTable.Cols.TITLE + ", " +
+        CrimeTable.Cols.DATE + ", " +
+        CrimeTable.Cols.SOLVED + ")");
 
     }
 

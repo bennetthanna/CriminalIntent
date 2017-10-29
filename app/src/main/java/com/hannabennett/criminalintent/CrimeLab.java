b60523b604d511d6783b7d7d1ce162ee46269f0f
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-29 11:03:44 -0700

    Chapter 16, page 310 : Add getPhotoFile(Crime) method that provides a complete local filepath for the crime's image

--- app/src/main/java/com/hannabennett/criminalintent/CrimeLab.java
+++ app/src/main/java/com/hannabennett/criminalintent/CrimeLab.java
@@ -9,6 +9,7 @@ import com.hannabennett.criminalintent.database.CrimeBaseHelper;
 import com.hannabennett.criminalintent.database.CrimeCursorWrapper;
 import com.hannabennett.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -73,6 +74,11 @@ public class CrimeLab {
         } finally {
             cursor.close();
         }
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFileName());
     }
 
     public void updateCrime(Crime crime) {

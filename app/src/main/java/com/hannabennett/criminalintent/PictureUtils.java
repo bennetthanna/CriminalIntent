d8c4502dc59e47a0e077f9596191c8cf7247a133
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-31 10:50:06 -0700

    Chapter 16, page 314 : Scale bitmap by scanning the file to see how big it is, figure out how much you need to scale it by, reread the file to create scaled-down Bitmap object

---
+++ app/src/main/java/com/hannabennett/criminalintent/PictureUtils.java
@@ -0,0 +1,35 @@
+package com.hannabennett.criminalintent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Created by HannaBennett on 10/31/17.
+ */
+
+public class PictureUtils {
+    public static Bitmap getScaledBitmap (String path, int destWidth, int destHeight) {
+        // read in the dimensions of the image on disk
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        // figure out how much to scale down by
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            float heightScale = srcHeight / destHeight;
+            float widthScale = srcWidth / destWidth;
+
+            inSampleSize = Math.round(heightScale > widthScale ? heightScale : widthScale);
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        // read in and create final bitmap
+        return BitmapFactory.decodeFile(path, options);
+    }
+}

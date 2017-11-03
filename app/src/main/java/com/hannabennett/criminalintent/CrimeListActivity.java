cfe7f86875bc55a7ec683a9e8dfa1e799508b7e4
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-03 14:04:46 -0700

    Chapter 17, page 329 : Implement callbacks and if layout has detail_fragment_container then put CrimeFragment in container or else start a new CrimePagerActivity

--- app/src/main/java/com/hannabennett/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/hannabennett/criminalintent/CrimeListActivity.java
@@ -1,12 +1,13 @@
 package com.hannabennett.criminalintent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by HannaBennett on 10/7/17.
  */
 
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
@@ -15,5 +16,18 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        if (findViewById(R.id.detail_fragment_container) == null) {
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+            startActivity(intent);
+        } else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+        }
     }
 }
